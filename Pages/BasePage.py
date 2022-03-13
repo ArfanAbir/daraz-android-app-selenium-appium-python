bc44483dc01895d520926b52a96c65c7579a1cf1
Md Nazrul Islam <75695509+ArfanAbir@users.noreply.github.com>
2022-03-13 17:49:32 +0600

    Add files via upload

---
+++ Pages/BasePage.py
@@ -0,0 +1,20 @@
+import unittest
+from appium import webdriver
+
+
+class BasePage(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4723/wd/hub',
+            desired_capabilities=dict(platformName='Android', automationName='UiAutomator2', platformVersion='12',
+                                      udid='emulator-5554', app='E:\PythonAppiumBDJobs\APK\daraz.apk')
+        )
+        print("Test Started.......")
+
+    def tearDown(self):
+        # self.driver.quit()
+        print("Test Completed")
+
+if __name__ == "__main__":
+    unittest.main()
